75a1a62bfcbadbfd023cead95ae34b1973c03849
Thales Henrique <thaalespb@hotmail.com>
2024-08-22 10:04:04 -0300

    removendo padding

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -14,7 +14,7 @@ const HomeTemplate: React.FC = () => {
                     <div className="container relative mx-auto">
                         <div className="items-center flex flex-wrap">
                             <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                                <div className="pr-12">
+                                <div className="">
                                     <h1 className="text-white font-semibold text-5xl">
                                         Super Sorteio
                                     </h1>
